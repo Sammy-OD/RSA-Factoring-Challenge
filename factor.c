7f15e1be9521ac9c12840b457641c280bac04f6c
Samuel Odunlami <odunlamisamuel@gmail.com>
2023-07-02 02:03:11 -0700

    new update

--- factor.c
+++ factor.c
@@ -2,8 +2,8 @@
 
 /**
  * main - main function
- *
- *
+ * @argc: param
+ * @argv: param
  * Return: void
  */
 int main(int argc, char *argv[])
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "Error: can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	while((line = getline(&buffer, &count, fptr)) != -1)
+	while ((line = getline(&buffer, &count, fptr)) != -1)
 	{
 		factorize(buffer);
 	}
